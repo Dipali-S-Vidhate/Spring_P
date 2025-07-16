56de16af2636a0f4241f36c980158f17805efc9e
Dipali Vidhate <dipalividhate01@gmail.com>
2025-07-16 19:47:08 +0530

    first commit

---
+++ src/main/java/com/Web_Project/SpringPApplication.java
@@ -0,0 +1,14 @@
+package com.Web_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPApplication.class, args);
+		System.out.println("welcome spring");
+	}
+
+}
---
+++ src/test/java/com/Web_Project/SpringPApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Web_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
